aeacc73b81440700d01fc02d7c72b2070aef293e
notandy82 <notandystanek@gmail.com>
2022-11-21 13:29:44 +0000

    Add user redirects

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -5,10 +5,20 @@ import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
-import { Form, Button, Image, Col, Row, Container, Alert } from "react-bootstrap";
+import {
+    Form,
+    Button,
+    Image,
+    Col,
+    Row,
+    Container,
+    Alert,
+} from "react-bootstrap";
 import axios from "axios";
+import { useRedirect } from "../../hooks/useRedirect";
 
 const SignUpForm = () => {
+    useRedirect('loggedIn');
     const [signUpData, setSignUpData] = useState({
         username: "",
         password1: "",
